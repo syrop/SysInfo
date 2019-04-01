d253ada0da9d88d12e3be1270287816a1740543d
wiktor <wiktor.nizio@gmail.com>
2019-04-02 00:02:33 +0200

    Changed the base theme.

--- myapplication/src/main/java/pl/org/seva/myapplication/main/MainFragment.kt
+++ myapplication/src/main/java/pl/org/seva/myapplication/main/MainFragment.kt
@@ -1,10 +1,12 @@
 package pl.org.seva.myapplication.main
 
 import android.annotation.SuppressLint
+import android.os.AsyncTask
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import io.reactivex.schedulers.Schedulers
 import kotlinx.coroutines.*
 import pl.org.seva.myapplication.R
 import pl.org.seva.myapplication.main.extension.inflate
@@ -13,6 +15,16 @@ class MainFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?) =
             inflate(R.layout.fragment_main, container)
+
+    suspend fun a() {
+
+        delay(10L)
+
+        coroutineScope {
+
+            val a = launch { "2" }
+        }
+    }
 
     @SuppressLint("SetTextI18n", "CheckResult")
     override fun onActivityCreated(savedInstanceState: Bundle?) {
@@ -26,11 +38,15 @@ class MainFragment : Fragment() {
             a = async { 1 }
             a.onAwait
 
+            val bocian = async { 1 }
 
+            Schedulers.io()
             coroutineScope {
+                val zaba = async { 2 }
 
             }
 
         }
     }
+
 }
