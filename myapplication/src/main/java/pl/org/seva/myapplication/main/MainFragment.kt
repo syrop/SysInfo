e33eb94e87e7095a5c2930d7fa70e69c55b236d6
wiktor <wiktor.nizio@gmail.com>
2019-04-14 21:43:52 +0200

    Called await() on io.reactivex.Single.

--- myapplication/src/main/java/pl/org/seva/myapplication/main/MainFragment.kt
+++ myapplication/src/main/java/pl/org/seva/myapplication/main/MainFragment.kt
@@ -5,6 +5,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import io.reactivex.Single
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.rx2.await
 import pl.org.seva.myapplication.R
 import pl.org.seva.myapplication.main.extension.getViewModel
 import pl.org.seva.myapplication.main.extension.inflate
@@ -18,6 +22,12 @@ class MainFragment : Fragment() {
     @SuppressLint("SetTextI18n", "CheckResult")
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
+
+        GlobalScope.launch {
+            val sin = Single.fromCallable { 10 }
+            sin.await()
+        }
+
 
         val vm = getViewModel<VM>()
         vm.ld.observe(this) { println("wiktor $it")}
--- myapplication/src/main/java/pl/org/seva/myapplication/main/VM.kt
+++ myapplication/src/main/java/pl/org/seva/myapplication/main/VM.kt
@@ -14,22 +14,26 @@ class VM : ViewModel() {
 
     private fun thread() = "${System.identityHashCode(Thread.currentThread())}"
 
-    suspend fun f2() = coroutineScope {
+    private suspend fun f2() = coroutineScope {
         println("wiktor wewnętrzny thread ${thread()} ${main()}")
     }
 
     fun identity() {
         viewModelScope.launch {
-            println("wiktor mój thread ${thread()} ${main()}")
-            f2()
-            withContext (Dispatchers.IO) {
-                println("wiktor nie mój thread ${thread()} ${main()}")
+            coroutineScope {
+                println("wiktor mój thread ${thread()} ${main()}")
                 f2()
-                delay(4000)
+                withContext(Dispatchers.IO) {
+                    println("wiktor nie mój thread ${thread()} ${main()}")
+                    f2()
+                    delay(4000)
+                }
+                println("wiktor mój thread ${thread()} ${main()}")
+                ld.value = 10
             }
-            println("wiktor mój thread ${thread()} ${main()}")
-            ld.value = 10
         }
+
+        println("wiktor canceled")
     }
 
     override fun onCleared() {
