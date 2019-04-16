cc174e340e99401252f4307d07bbadd1fd2859c4
wiktor <wiktor.nizio@gmail.com>
2019-04-16 07:20:59 +0200

    New AS and Gradle.

--- myapplication/src/main/java/pl/org/seva/myapplication/main/MainFragment.kt
+++ myapplication/src/main/java/pl/org/seva/myapplication/main/MainFragment.kt
@@ -5,32 +5,40 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleEventObserver
+import androidx.lifecycle.viewModelScope
+import io.reactivex.Observable
 import io.reactivex.Single
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.*
 import kotlinx.coroutines.rx2.await
+import kotlinx.coroutines.rx2.rxCompletable
 import pl.org.seva.myapplication.R
 import pl.org.seva.myapplication.main.extension.getViewModel
 import pl.org.seva.myapplication.main.extension.inflate
 import pl.org.seva.myapplication.main.extension.observe
+import java.lang.Exception
+import java.util.*
 
 class MainFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?) =
             inflate(R.layout.fr_main, container)
 
+
+
     @SuppressLint("SetTextI18n", "CheckResult")
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
 
-        GlobalScope.launch {
-            val sin = Single.fromCallable { 10 }
-            sin.await()
+        val a = GlobalScope.async {
+            println("wiktor I am two")
+            2
         }
 
 
-        val vm = getViewModel<VM>()
-        vm.ld.observe(this) { println("wiktor $it")}
-        vm.identity()
+        GlobalScope.launch {
+
+        }
     }
 }
--- myapplication/src/main/java/pl/org/seva/myapplication/main/VM.kt
+++ myapplication/src/main/java/pl/org/seva/myapplication/main/VM.kt
@@ -8,36 +8,5 @@ import kotlinx.coroutines.*
 
 class VM : ViewModel() {
 
-    val ld = MutableLiveData<Int>()
 
-    private fun main() = Looper.getMainLooper().thread === Thread.currentThread()
-
-    private fun thread() = "${System.identityHashCode(Thread.currentThread())}"
-
-    private suspend fun f2() = coroutineScope {
-        println("wiktor wewnętrzny thread ${thread()} ${main()}")
-    }
-
-    fun identity() {
-        viewModelScope.launch {
-            coroutineScope {
-                println("wiktor mój thread ${thread()} ${main()}")
-                f2()
-                withContext(Dispatchers.IO) {
-                    println("wiktor nie mój thread ${thread()} ${main()}")
-                    f2()
-                    delay(4000)
-                }
-                println("wiktor mój thread ${thread()} ${main()}")
-                ld.value = 10
-            }
-        }
-
-        println("wiktor canceled")
-    }
-
-    override fun onCleared() {
-        super.onCleared()
-        println("wiktor cleared")
-    }
 }
