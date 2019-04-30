f2f29b4e727c1770b72f010c7508a2628a0964ab
wiktor <wiktor.nizio@gmail.com>
2019-04-30 05:56:10 +0200

    Tested canceling channels.

--- myapplication/src/main/java/pl/org/seva/myapplication/main/MainFragment.kt
+++ myapplication/src/main/java/pl/org/seva/myapplication/main/MainFragment.kt
@@ -2,13 +2,43 @@ package pl.org.seva.myapplication.main
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
+import kotlinx.coroutines.*
+import kotlinx.coroutines.channels.BroadcastChannel
+import kotlinx.coroutines.channels.Channel
 import pl.org.seva.myapplication.R
 
 class MainFragment : Fragment(R.layout.fr_main) {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
+        val ch1 = Channel<Int>()
+        val ch2 = Channel<Int>()
+        val br1 = BroadcastChannel<Int>(Channel.CONFLATED)
+        br1.openSubscription()
+        ch1.close()
 
+        GlobalScope.launch {
 
+            coroutineScope {
+                val i = launch {
+                    try {
+                        println("wiktor 0")
+                        delay(500)
+                        println("wiktor a")
+                    }
+                    finally {
+                        println("wiktor b")
+                    }
+                }
+                i.cancel()
+            }
+
+        }
+        GlobalScope.launch {
+            for (a in 1..10) {
+                ch1.send(a)
+                ch2.send(a)
+            }
+        }
     }
 }
