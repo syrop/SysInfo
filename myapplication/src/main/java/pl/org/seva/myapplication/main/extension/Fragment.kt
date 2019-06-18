abd4bc064dd4f4349bad990bbadd1ae86ff37a54
wiktor <wiktor.nizio@gmail.com>
2019-06-18 10:34:57 +0200

    Added an extension function.

--- myapplication/src/main/java/pl/org/seva/myapplication/main/extension/Fragment.kt
+++ myapplication/src/main/java/pl/org/seva/myapplication/main/extension/Fragment.kt
@@ -2,7 +2,9 @@ package pl.org.seva.myapplication.main.extension
 
 import android.content.Context
 import android.content.SharedPreferences
+import android.content.pm.PackageManager
 import androidx.annotation.IdRes
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModel
 import androidx.navigation.fragment.findNavController
@@ -20,3 +22,6 @@ inline fun <reified R : ViewModel> Fragment.getViewModel() = activity!!.getViewM
 
 fun Fragment.prefs(name: String): SharedPreferences =
         requireContext().getSharedPreferences(name, Context.MODE_PRIVATE)
+
+fun Fragment.checkPermission(permission: String) =
+        ContextCompat.checkSelfPermission(context!!, permission) == PackageManager.PERMISSION_GRANTED
