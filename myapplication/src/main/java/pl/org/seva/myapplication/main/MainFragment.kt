9597aeaec7f732abff761297b4ca2b227f788c3c
wiktor <wiktor.nizio@gmail.com>
2019-06-30 12:38:08 +0200

    Removed Room dependency.

--- myapplication/src/main/java/pl/org/seva/myapplication/main/MainFragment.kt
+++ myapplication/src/main/java/pl/org/seva/myapplication/main/MainFragment.kt
@@ -1,41 +1,44 @@
 package pl.org.seva.myapplication.main
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.LiveData
 import androidx.navigation.navGraphViewModels
+import com.google.android.gms.maps.model.LatLng
+import io.reactivex.Single
+import io.reactivex.rxkotlin.Singles
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 import pl.org.seva.myapplication.R
 import pl.org.seva.myapplication.main.extension.invoke
-
-object A
-
 
 class MainFragment : Fragment(R.layout.fr_main) {
 
     private val vm by navGraphViewModels<VM>(R.id.nav_graph)
 
+    @SuppressLint("CheckResult")
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
 
-        (vm.ld to this) {
-            println("wiktor result $it")
-        }
+        var a = 10
+        val f = { a++ }
+        f()
+        f()
+        println("wiktor $a")
 
-        (MyLiveData() to this) {
-
-        }
-    }
-
-    class MyLiveData : LiveData<Int>() {
-
-        override fun onActive() {
-            super.onActive()
-            println("wiktor is active")
-        }
-
-        override fun onInactive() {
-            super.onInactive()
-            println("wiktor is inactive")
-        }
     }
 }
+
+class LastLocationPresenter(lastLocationView: (LatLng?) -> Unit) {
+
+    init {
+        GlobalScope.launch {
+            lastLocationView(getLastLocation())
+        }
+    }
+
+    private suspend fun getLastLocation(): LatLng? = null
+
+}
+
