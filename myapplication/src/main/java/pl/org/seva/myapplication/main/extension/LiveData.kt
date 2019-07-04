90f5c9f152f3d00a02ec2cb04558e8c3886564b8
wiktor <wiktor.nizio@gmail.com>
2019-07-04 15:09:04 +0200

    Removed a redundant function.

--- myapplication/src/main/java/pl/org/seva/myapplication/main/extension/LiveData.kt
+++ myapplication/src/main/java/pl/org/seva/myapplication/main/extension/LiveData.kt
@@ -2,11 +2,7 @@ package pl.org.seva.myapplication.main.extension
 
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.LiveData
-import androidx.lifecycle.Observer
+import androidx.lifecycle.observe
 
 operator fun <T> LiveData<T>.invoke(owner: LifecycleOwner, observer: (T) -> Unit) =
         observe(owner) { observer(it) }
-
-@Suppress("unused")
-fun <T> LiveData<T>.observe(owner: LifecycleOwner, observer: (T) -> Unit) =
-        observe(owner, Observer<T> { observer(it) })
