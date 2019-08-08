75f3a91f65a1011cb71d887de6c5d9acc9c2d888
wiktor <wiktor.nizio@gmail.com>
2019-08-08 12:15:37 +0200

    Used an Android-specific library.

--- myapplication/src/main/java/pl/org/seva/myapplication/main/MainFragment.kt
+++ myapplication/src/main/java/pl/org/seva/myapplication/main/MainFragment.kt
@@ -3,43 +3,17 @@ package pl.org.seva.myapplication.main
 import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.LiveData
-import io.reactivex.Observable
-import io.reactivex.disposables.Disposable
-import io.reactivex.subjects.PublishSubject
 import pl.org.seva.myapplication.R
-import pl.org.seva.myapplication.main.extension.plus
 
 class MainFragment : Fragment(R.layout.fr_main) {
 
-    inner class RxLiveData<T>(private val observable: Observable<T>) : LiveData<T>() {
-
-        private lateinit var disposable: Disposable
-
-        override fun onActive() {
-            super.onActive()
-            println("Wiktor state active: " + lifecycle.currentState)
-            Thread.dumpStack()
-            disposable = observable.subscribe { postValue(it) }
-        }
-
-        override fun onInactive() {
-            super.onInactive()
-            println("wiktor state inactive: ${lifecycle.currentState}")
-            Thread.dumpStack()
-            disposable.dispose()
-        }
-    }
 
     @SuppressLint("CheckResult")
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
 
-        val obs = PublishSubject.create<Int>()
 
-        val ld = RxLiveData(obs)
 
-        (ld + this) { println("wiktor fajnie ")}
 
     }
 }
