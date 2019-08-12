3082bb34ce579c5cd2479e5660a508dde40b2b01
wiktor <wiktor.nizio@gmail.com>
2019-08-12 18:12:09 +0200

    New libraries.

--- myapplication/src/main/java/pl/org/seva/myapplication/main/extension/FragmentActivity.kt
+++ myapplication/src/main/java/pl/org/seva/myapplication/main/extension/FragmentActivity.kt
@@ -2,9 +2,9 @@ package pl.org.seva.myapplication.main.extension
 
 import androidx.fragment.app.FragmentActivity
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.ViewModelProviders
+import androidx.lifecycle.ViewModelProvider
 
 inline fun <reified R : ViewModel> FragmentActivity.viewModel() = lazy { getViewModel<R>() }
 
 inline fun <reified R : ViewModel> FragmentActivity.getViewModel() =
-        ViewModelProviders.of(this).get(R::class.java)
+        ViewModelProvider(this).get(R::class.java)
