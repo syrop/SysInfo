7f5594da2a9f67a64387ef4f76651d42542ad1e0
wiktor <wiktor.nizio@gmail.com>
2019-08-19 19:40:59 +0200

    Tested pressing [Back].

--- myapplication/src/main/java/pl/org/seva/myapplication/main/MainFragment.kt
+++ myapplication/src/main/java/pl/org/seva/myapplication/main/MainFragment.kt
@@ -2,8 +2,12 @@ package pl.org.seva.myapplication.main
 
 import android.annotation.SuppressLint
 import android.os.Bundle
+import androidx.activity.addCallback
 import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fr_main.*
 import pl.org.seva.myapplication.R
+import pl.org.seva.myapplication.main.extension.invoke
+import pl.org.seva.myapplication.main.extension.nav
 
 class MainFragment : Fragment(R.layout.fr_main) {
 
@@ -11,5 +15,12 @@ class MainFragment : Fragment(R.layout.fr_main) {
     @SuppressLint("CheckResult")
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
+
+        next { nav(R.id.action_mainFragment_to_secondFragment) }
+
+        requireActivity().onBackPressedDispatcher.addCallback(this) {
+            // Handle the back button event
+            println("wiktor back main")
+        }
     }
 }
--- myapplication/src/main/java/pl/org/seva/myapplication/main/SecondFragment.kt
+++ myapplication/src/main/java/pl/org/seva/myapplication/main/SecondFragment.kt
@@ -1,6 +1,7 @@
 package pl.org.seva.myapplication.main
 
 import android.os.Bundle
+import androidx.activity.addCallback
 import androidx.fragment.app.Fragment
 import kotlinx.android.synthetic.main.fr_main.*
 import pl.org.seva.myapplication.R
@@ -12,5 +13,10 @@ class SecondFragment : Fragment(R.layout.fr_main) {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         next { nav(R.id.action_secondFragment_to_thirdFragment) }
+
+        requireActivity().onBackPressedDispatcher.addCallback(this) {
+            // Handle the back button event
+            println("wiktor second back")
+        }
     }
 }
