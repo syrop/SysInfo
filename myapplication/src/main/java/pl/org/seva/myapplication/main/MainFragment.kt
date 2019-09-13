17c0975596e8c6c9f5e84bbecc6d8a37a9e3ac5e
wiktor <wiktor.nizio@gmail.com>
2019-09-13 12:34:42 +0200

    Tested removing observers.

--- myapplication/src/main/java/pl/org/seva/myapplication/main/MainFragment.kt
+++ myapplication/src/main/java/pl/org/seva/myapplication/main/MainFragment.kt
@@ -3,7 +3,14 @@ package pl.org.seva.myapplication.main
 import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Observer
 import kotlinx.android.synthetic.main.fr_main.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import pl.org.seva.myapplication.R
 import pl.org.seva.myapplication.main.extension.invoke
 import pl.org.seva.myapplication.main.extension.nav
@@ -15,14 +22,28 @@ class MainFragment : Fragment(R.layout.fr_main) {
         super.onActivityCreated(savedInstanceState)
         next { nav(R.id.action_mainFragment_to_secondFragment) }
 
-        for (id in 1 until 10) {
-            println("wiktor $id")
+        val wiktorLiveData = object : LiveData<Unit>() {
+            override fun onActive() {
+                println("wiktor active")
+            }
+
+            override fun onInactive() {
+                println("wiktor inactive")
+
+            }
         }
 
-        val to = 1
-        for (id in 10 downTo to + 1) {
-            println("wiktor misia $id")
+        fun <T> LiveData<T>.reObserve(owner: LifecycleOwner, observer: Observer<T>) {
+            println("wiktor ${System.identityHashCode(observer)}")
+            removeObserver(observer)
+            observe(owner, observer)
         }
 
+        fun zaba() {
+            wiktorLiveData.reObserve(this, Observer { "aa" })
+        }
+
+        zaba()
+        zaba()
     }
 }
