409aef1d177da990715b1b086bec0f73589cce0d
wiktor <wiktor.nizio@gmail.com>
2019-09-29 19:29:56 +0200

    Tested synchronization.

--- myapplication/src/main/java/pl/org/seva/myapplication/main/MainFragment.kt
+++ myapplication/src/main/java/pl/org/seva/myapplication/main/MainFragment.kt
@@ -1,25 +1,57 @@
 package pl.org.seva.myapplication.main
 
-import android.annotation.SuppressLint
 import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.MutableLiveData
-import kotlinx.android.synthetic.main.fr_main.*
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.suspendCancellableCoroutine
+import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.withLock
+import kotlinx.coroutines.withContext
 import pl.org.seva.myapplication.R
-import pl.org.seva.myapplication.main.extension.invoke
-import pl.org.seva.myapplication.main.extension.nav
+import java.time.Duration
 
 class MainFragment : Fragment(R.layout.fr_main) {
 
-    @SuppressLint("CheckResult")
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        next { nav(R.id.action_mainFragment_to_secondFragment) }
+    private val mux = Mutex()
 
-        val a = MutableLiveData<Int>()
-        a.postValue(10)
-        println("wiktor ${a.value}")
-        a.postValue(20)
-        println("wiktor ${a.value}")
+    private fun synchronizeThis(callback: () -> Unit) {
+        println("wiktor begin to sleep")
+        Thread.sleep(Duration.ofSeconds(2).toMillis())
+        println("wiktor slept")
+        callback()
+    }
+
+
+    private suspend fun wrappedSynchronizeThis() = withContext(Dispatchers.IO) {
+        suspendCancellableCoroutine<Unit> { continuation ->
+            synchronizeThis {
+                continuation.resumeWith(Result.success(Unit))
+            }
+        }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        lifecycleScope.launch {
+            mux.withLock {
+                wrappedSynchronizeThis()
+            }
+        }
+        println("wiktor finished onViewCreated")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        println("wiktor on resume")
+        lifecycleScope.launch {
+            mux.withLock {
+                println("wiktor in the synchronized block in onResume()")
+            }
+        }
+        println("wiktor finished onResume()")
     }
 }
