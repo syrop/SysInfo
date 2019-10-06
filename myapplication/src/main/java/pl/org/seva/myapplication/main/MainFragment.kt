e426bf69ac22637a3ac860c1112db21aa41d8584
wiktor <wiktor.nizio@gmail.com>
2019-10-06 10:08:57 +0200

    New Kodein.

--- myapplication/src/main/java/pl/org/seva/myapplication/main/MainFragment.kt
+++ myapplication/src/main/java/pl/org/seva/myapplication/main/MainFragment.kt
@@ -8,31 +8,28 @@ import kotlinx.coroutines.*
 import kotlinx.coroutines.channels.BroadcastChannel
 import kotlinx.coroutines.channels.Channel
 import pl.org.seva.myapplication.R
+import java.util.concurrent.CancellationException
 
 @ExperimentalCoroutinesApi
 class MainFragment : Fragment(R.layout.fr_main) {
 
     private val ch = BroadcastChannel<Boolean>(Channel.CONFLATED)
 
+    private suspend fun s() = suspendCancellableCoroutine<Unit> {
+        it.cancel()
+        println("wiktor after cancel")
+    }
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         lifecycleScope.launch {
-            delay(3000)
-            ch.offer(true)
+            try {
+                s()
+                println("wiktor no exception")
+            } catch (e: CancellationException) {
+                println("wiktor exception")
+            }
         }
-    }
 
-    override fun onResume() {
-        super.onResume()
-
-        lifecycleScope.launch {
-            val ch = ch.openSubscription()
-            println("wiktor waiting for")
-            val received = ch.receive()
-            println("wiktor received: $received")
-            ch.cancel()
-        }
-        println("wiktor finished onResume")
     }
 }
