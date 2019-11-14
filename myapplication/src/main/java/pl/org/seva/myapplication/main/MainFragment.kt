ef3a7e4bc323ed6c1cb21c478a1f274f3b63409d
wiktor <wiktor.nizio@gmail.com>
2019-11-14 16:00:41 +0100

    Tested "object" keyword.

--- myapplication/src/main/java/pl/org/seva/myapplication/main/MainFragment.kt
+++ myapplication/src/main/java/pl/org/seva/myapplication/main/MainFragment.kt
@@ -3,12 +3,10 @@ package pl.org.seva.myapplication.main
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.lifecycleScope
 import kotlinx.coroutines.*
 import kotlinx.coroutines.channels.BroadcastChannel
 import kotlinx.coroutines.channels.Channel
 import pl.org.seva.myapplication.R
-import java.util.concurrent.CancellationException
 
 @ExperimentalCoroutinesApi
 class MainFragment : Fragment(R.layout.fr_main) {
@@ -22,13 +20,12 @@ class MainFragment : Fragment(R.layout.fr_main) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        lifecycleScope.launch {
-            try {
-                s()
-                println("wiktor no exception")
-            } catch (e: CancellationException) {
-                println("wiktor exception")
-            }
+    }
+
+    object Obj {
+
+        init {
+            println("wiktor object")
         }
     }
 }
